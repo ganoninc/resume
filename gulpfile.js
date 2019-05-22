b53638087e4c8d2670acd04cd17d00488e0461b7
Romain Giovanetti <ganoninc@gmail.com>
2019-05-21 20:36:31 -0400

    migrate to gulp 4.x

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,17 @@
-var gulp = require('gulp');
+/*jshint esversion: 6 */
+
+/* 
+    Build the assets and can start a local webserver with live reload
+
+    gulp start  // start the webserver and reload automaticly when needed
+    gulp build  // build the assets
+
+    Author: Romain Giovanetti
+
+    Gulp 4.x is required
+*/
+
+const { watch, parallel, series, src, dest } = require('gulp');
 var plumber = require('gulp-plumber');
 var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
@@ -6,9 +19,8 @@ var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 var webserver = require('gulp-webserver');
 
-
-gulp.task('scripts', function() {
-    return gulp.src('js/scripts.js')
+function buildScriptsTask() {
+    return src('js/scripts.js')
         .pipe(plumber(plumber({
             errorHandler: function (err) {
                 console.log(err);
@@ -16,29 +28,41 @@ gulp.task('scripts', function() {
             }
         })))
         .pipe(uglify())
-        .pipe(rename({extname: '.min.js'}))
-        .pipe(gulp.dest('js'));
-});
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('js'));
+}
 
-gulp.task('styles', function() {
-    return gulp.src('scss/styles.scss')
+function watchScriptsTask() {
+    return watch('js/scripts.js', function () {
+        series(buildScriptsTask);
+    });
+}
+
+function buildStylesTask() {
+    return src('scss/styles.scss')
         .pipe(plumber(plumber({
             errorHandler: function (err) {
                 console.log(err);
                 this.emit('end');
             }
         })))
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(autoprefixer())
-        .pipe(gulp.dest('css'));
-});
+        .pipe(dest('css'));
+}
 
-gulp.task('webserver', function() {
-    return gulp.src('./')
+function watchStylesTask() {
+    return watch('scss/*.scss', function () {
+        series(buildStylesTask);
+    });
+}
+
+function webserverTask() {
+    return src('./')
         .pipe(webserver({
             livereload: true,
             directoryListing: true,
-            open: true,
+            open: 'http://localhost:8000/index.html',
             enable: true, // need this set to true to enable livereload 
             filter: function(fileName) {
                 if (fileName.match(/.scss$/)) {
@@ -48,9 +72,23 @@ gulp.task('webserver', function() {
                 }
             }
         }));
-});
+}
 
-gulp.task('watch', gulp.series('scripts', 'styles', 'webserver'), function() {
-    gulp.watch('js/scripts.js', ['scripts']);
-    gulp.watch('scss/*.scss', ['styles']);
-});
+function buildTask(cb) {
+    buildStylesTask();
+    buildScriptsTask();
+    if(cb)
+        cb();
+}
+
+
+function watchTask(cb) {
+    buildTask();
+    webserverTask();
+    parallel(watchScriptsTask, watchStylesTask);
+    cb();
+}
+
+exports.default = watchTask;
+exports.start = watchTask;
+exports.build = buildTask;
